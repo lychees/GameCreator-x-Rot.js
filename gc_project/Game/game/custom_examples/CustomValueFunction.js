6f739a0da4f46f6cafa968865a7aefa188d318da
xiaodao <lychees67@gmail.com>
2021-07-20 19:37:28 +0800

    按 shift 可原地转向

--- gc_project/Game/game/custom_examples/CustomValueFunction.js
+++
@@ -1,8 +0,0 @@
-var CustomValueFunction;
-(function (CustomValueFunction) {
-    function f1(trigger, triggerPlayer) {
-        return (new Date()).getHours();
-    }
-    CustomValueFunction.f1 = f1;
-})(CustomValueFunction || (CustomValueFunction = {}));
-//# sourceMappingURL=CustomValueFunction.js.map
